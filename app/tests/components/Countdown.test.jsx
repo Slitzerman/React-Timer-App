6ed851d82443ae3ec42c3fafda3c2acef08bd6db
Niko Pinnis <niko.pinnis@gmail.com>
2017-08-17 20:51:54 +0300

    Finalise tests and add page-title

--- app/tests/components/Countdown.test.jsx
+++ app/tests/components/Countdown.test.jsx
@@ -42,5 +42,27 @@ describe('Countdown', () => {
         done();
       }, 2010);
     });
+    it('Should pause countdown on paused status', () => {
+      var countdown = TestUtils.renderIntoDocument(<Countdown />);
+      countdown.handleSetCountdown(3);
+      countdown.handleStatusChange('paused');
+
+      setTimeout(() => {
+        expect(countdown.state.count).toBe(3);
+        expect(countdown.state.countdownStatus).toBe('paused');
+        done();
+      }, 1010);
+    });
+    it('Should have stopped status when countdown ends', () => {
+      var countdown = TestUtils.renderIntoDocument(<Countdown />);
+      countdown.handleSetCountdown(3);
+      countdown.handleStatusChange('stopped');
+
+      setTimeout(() => {
+        expect(countdown.state.count).toBe(0);
+        expect(countdown.state.countdownStatus).toBe('stopped');
+        done();
+      }, 1010);
+    });
   });
 });
