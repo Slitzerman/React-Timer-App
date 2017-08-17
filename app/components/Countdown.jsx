8f64105ff6e7fc8b12e88a902ec31b9af3dd0bf4
Niko Pinnis <niko.pinnis@gmail.com>
2017-08-17 20:16:57 +0300

    Update status to stop count reaches 0

--- app/components/Countdown.jsx
+++ app/components/Countdown.jsx
@@ -32,6 +32,12 @@ var Countdown = React.createClass({
     this.timer = setInterval(() => {
       var newCount = this.state.count - 1;
       this.setState({ count: newCount >= 0 ? newCount : 0 });
+
+      if (newCount === 0) {
+        this.setState({
+          countdownStatus: 'stopped'
+        });
+      }
     }, 1000);
   },
   handleSetCountdown: function(seconds) {
