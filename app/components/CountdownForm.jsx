d6dd5d10d7980401c76436171b3c61a1b6006d99
Niko Pinnis <niko.pinnis@gmail.com>
2017-08-18 21:22:26 +0300

    Fix bug in Countdown when input was empty

--- app/components/CountdownForm.jsx
+++ app/components/CountdownForm.jsx
@@ -5,6 +5,9 @@ var CountdownForm = React.createClass({
   onSubmit: function(e) {
     e.preventDefault();
     var strSeconds = this.refs.seconds.value;
+    if (strSeconds === '') {
+      return;
+    }
     if (strSeconds.match(/^[0-9]*$/)) {
       this.refs.seconds.value = '';
       this.props.onSetCountdown(parseInt(strSeconds, 10));
